6979c209cbb0a581c505ed05b8d071afc505838a
coltonb <14205996+coltonb@users.noreply.github.com>
2023-06-19 23:37:16 -0400

    make team client side for reactivity

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -2,11 +2,11 @@ import prisma from "@/lib/prisma";
 import Team from "@/components/team";
 
 export default async function Teams() {
-  const teams = await prisma.teams.findMany();
+  const teams = await prisma.teams.findMany({ include: { users: true } });
   return (
     <div className="flex w-full space-x-3">
       {teams.map((team) => {
-        return <Team key={team.id} id={team.id} />;
+        return <Team key={team.id} team={team} />;
       })}
     </div>
   );
--- components/team.tsx
+++ components/team.tsx
@@ -1,27 +1,40 @@
-import prisma from "@/lib/prisma";
-import { selectColor } from "@/lib/utils";
-import { ClipboardDocumentIcon } from "@heroicons/react/24/outline";
+"use client";
+
+import { teams, users } from "@prisma/client";
+import {
+  ClipboardDocumentIcon,
+  XCircleIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { useDebounce } from "react-use";
 
 interface TeamProps {
-  id: number;
+  team: teams & { users: users[] };
 }
 
-export default async function Team(props: TeamProps) {
-  const team = await prisma.teams.findFirst({
-    where: { id: props.id },
-    include: { users: true },
-  });
+export default function Team(props: TeamProps) {
+  const [score, setScore] = useState(props.team.score);
+  const [debouncedScore, setDebouncedScore] = useState(score);
 
-  if (!team) {
-    return <div />;
-  }
+  const [, cancel] = useDebounce(
+    () => {
+      if (score === debouncedScore) {
+        return;
+      }
+
+      console.log(score);
+      setDebouncedScore(score);
+    },
+    2000,
+    [score]
+  );
 
   return (
     <div className="card card-compact w-96 shadow-xl">
       <div className="card-body">
         <h2 className="card-title">
           <span className="flex-1">
-            <span>{team.name}</span>
+            <span>{props.team.name}</span>
             <div className="tooltip" data-tip="Copy Team Code">
               <button className="pl-1">
                 <ClipboardDocumentIcon className="inline w-5" />
@@ -29,12 +42,28 @@ export default async function Team(props: TeamProps) {
             </div>
           </span>
 
-          <span>Score: {team.score}</span>
+          <span>
+            Score:
+            <input
+              type="text"
+              maxLength={3}
+              className="pl-1 w-10 bg-transparent focus:underline flex-1 focus:outline-none"
+              value={score}
+              onChange={(e) => {
+                setScore(parseInt(e.target.value) || 0);
+              }}
+            />
+          </span>
+          <div className="tooltip" data-tip="Delete Team">
+            <button>
+              <XCircleIcon className="inline w-5" />
+            </button>
+          </div>
         </h2>
         <hr />
         <table className="table table-xs table-zebra">
           <tbody>
-            {team.users.map((user) => (
+            {props.team.users.map((user) => (
               <tr key={user.id}>
                 <td>{user.name}</td>
               </tr>
