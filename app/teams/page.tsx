52bc988a2f67970733d548d0bafc5732b16983fc
coltonb <14205996+coltonb@users.noreply.github.com>
2023-06-20 11:17:27 -0400

    force teams dynamic

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import Teams from "@/components/teams";
 
+export const dynamic = "force-dynamic";
+
 export default async function TeamsPage() {
   const teams = await prisma.team.findMany({ include: { users: true } });
   return <Teams teams={teams} />;
